dc0f9bd6aa69d5ccac27d9af5c9e4260f483ae2f
dawid9f <97540653+dawid9f@users.noreply.github.com>
2023-03-06 21:58:38 +0100

    SignUpValidation

--- Application/Functions/Auth/Command/SignUp/SignUpCommandValidator.cs
+++ Application/Functions/Auth/Command/SignUp/SignUpCommandValidator.cs
@@ -22,6 +22,48 @@ namespace Application.Functions.Auth.Command.SignUp
                 .WithMessage("Musisz podać hasło")
                 .MinimumLength(8)
                 .WithMessage("Hasło musi zawierać 8 znaków");
+
+            RuleFor(x => x.PersonalData.Country)
+                .NotEmpty()
+                .WithMessage("Musisz podać państwo");
+
+            RuleFor(x => x.PersonalData.City)
+                .NotEmpty()
+                .WithMessage("Musisz podać miasto");
+
+            RuleFor(x => x.PersonalData.Street)
+                .NotEmpty()
+                .WithMessage("Musisz podać ulicę");
+
+            RuleFor(x => x.PersonalData.HouseNumber)
+                .NotEmpty()
+                .WithMessage("Musisz podać numer domu");
+
+            RuleFor(x => x.PersonalData.PhoneNumber)
+                .NotEmpty()
+                .WithMessage("Musisz podać numer telefonu");
+
+            RuleFor(x => x.PersonalData.PostalCode)
+                .NotEmpty()
+                .WithMessage("Musisz podać kod pocztowy");
+
+            RuleFor(x => x.PersonalData.PESEL)
+                .NotEmpty()
+                .WithMessage("Musisz podać kod PESEL");
+
+            RuleFor(x => x.PersonalData.FirstName)
+                .NotEmpty()
+                .WithMessage("Musisz podać imię");
+
+            RuleFor(x => x.PersonalData.LastName)
+                .NotEmpty()
+                .WithMessage("Musisz podać nazwisko");
+
+            RuleFor(x => x.PersonalData.DateOfBirth)
+                .NotEmpty()
+                .WithMessage("Musisz podać datę urodzenia")
+                .LessThan(DateTime.UtcNow.AddYears(-5))
+                .WithMessage("Podaj poprawną datę");
         }
     }
 }
